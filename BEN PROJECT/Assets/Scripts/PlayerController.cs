48b791a94968f5f6ef1189d8335f4759b3b3d014
BenKugges <19287@wc.school.nz>
2022-04-23 12:11:53 +1200

    On Collision Stop Spawning Enemies

--- BEN PROJECT/Assets/Scripts/PlayerController.cs
+++ BEN PROJECT/Assets/Scripts/PlayerController.cs
@@ -38,7 +38,14 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
             Debug.Log("Game Over!");
         }
+
+
+
     }
 }
--- BEN PROJECT/Assets/Scripts/SpawnManager.cs
+++ BEN PROJECT/Assets/Scripts/SpawnManager.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    private PlayerController playerControllerScript;
     public GameObject[] enemyPrefabs;
     public int enemyIndex;
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
+       
+        {
+            
+        }
+    }
+
+    void SpawnObstacle ()
+    {
+        if (playerControllerScript.gameOver == false)
         {
             int enemyIndex = Random.Range(0, enemyPrefabs.Length);
             Instantiate(enemyPrefabs[enemyIndex], new Vector3(21, 0, 9), enemyPrefabs[enemyIndex].transform.rotation);
         }
+        
     }
+
+
+
 }
+
+
