fe0bc2205d7dabb3ca52a61715db6fc13874a5fb
BenKugges <19287@wc.school.nz>
2022-05-04 18:49:21 +1200

    Game Over Screen Finished

--- BEN PROJECT/Assets/Scripts/GameOverScreen.cs
+++ BEN PROJECT/Assets/Scripts/GameOverScreen.cs
@@ -1,31 +1,38 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameOverScreen : MonoBehaviour
 {
+    //Private PlayerController playerControllerScript is used to find the asset with the playercontroller script attached//
+    //The public GameObject endScreen is the asset that will be displayed when the player loses//
     private PlayerController playerControllerScript;
     public GameObject endScreen;
 
     // Start is called before the first frame update
+    //This finds the gameObject with named "Player"//
     void Start()
     {
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
+
+    //
     void Update()
     {
         if (playerControllerScript.gameOver == true)
         {
 
-            Instantiate(endScreen, new Vector3(-2, 2, -2), endScreen.transform.rotation);
+            Instantiate(endScreen, new Vector3(-1, 2, 10), endScreen.transform.rotation);
+            Destroy(gameObject);
         }
 
     }
 
- 
-        
+
+
 
 
 }
